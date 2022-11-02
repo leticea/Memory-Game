792907dec861bd5d21944013e52d4c2f690bb227
leticea <nascimento.leticeah@gmail.com>
2022-11-02 15:45:23 -0300

    inserindo função validar input

--- js/login.js
+++ js/login.js
@@ -6,7 +6,10 @@ const validateInput = ({ target }) => {
     if (target.value.length > 2) {
 
         button.removeAttribute('disabled');
+        return;
     }
+
+    button.setAttribute('disabled', '');    
 }
 
 input.addEventListener('input', validateInput);
