cd23a94d7af85fef07c9e87629fc561c0ab6911b
leticea <nascimento.leticeah@gmail.com>
2022-11-03 16:28:58 -0300

    criando o README

--- js/game.js
+++ js/game.js
@@ -49,7 +49,7 @@ const checkEndGame = () => {
     if (disabledCards.length === 20) {
 
         clearInterval(this.loop);
-        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}.`);
     }
 };
 
